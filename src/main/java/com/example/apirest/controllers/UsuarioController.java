bd80d1dd622fac7266addf3b4dd5ff581eca5d54
carlosMorenoQ <92289007+carlosMorenoQ@users.noreply.github.com>
2021-11-23 09:40:36 -0500

    Commit 4: Se crea el restController en UsuarioController.java y se comprueba funcionamiento correcto

--- src/main/java/com/example/apirest/controllers/UsuarioController.java
+++ src/main/java/com/example/apirest/controllers/UsuarioController.java
@@ -1,4 +1,26 @@
 package com.example.apirest.controllers;
 
+import com.example.apirest.models.UsuarioModel;
+import com.example.apirest.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
+
 }
